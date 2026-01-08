f4b1e22e84877afc7e6837c9ec02b632c7a1fa39
Marika <mscrepnek2004@gmail.com>
2026-01-08 12:13:54 -0800

    add simple nav bar, with user-settings-icon stored in src/assets

--- BeautyBuddy-frontend/src/App.jsx
+++ BeautyBuddy-frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import ProductList from './components/ProductList';
 
+import userSettingsIcon from './assets/images/user-settings-icon.png';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <header className="navigation-bar">
-        <h1>BeautyBuddy</h1>
+        <h1 className='logo'>BeautyBuddy</h1>
+
+        <nav className='nav-bar-links'>
+          <span>Home</span>
+          <span>My Routines</span>
+          <span>Community</span>
+        </nav>
+
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search products..."
+        />
+
+        <img
+          className="user-settings-icon"
+          src={userSettingsIcon}
+          alt="User settings"
+        />
       </header>
       <ProductList />
     </div>
@@ -14,3 +34,6 @@ function App() {
 }
 
 export default App
+
+
+/*attribution for user setting icon (put at bottom of page): <a href="https://www.flaticon.com/free-icons/setting" title="setting icons">Setting icons created by Tanah Basah - Flaticon</a>*/
