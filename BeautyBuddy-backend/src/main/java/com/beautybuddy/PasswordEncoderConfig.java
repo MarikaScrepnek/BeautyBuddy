7ca5a085e4fac8529709236f67f2e062b5237c9a
Marika <mscrepnek2004@gmail.com>
2026-01-14 17:14:44 -0800

    rename passwordencoderconfig file to make more sense

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/PasswordEncoderConfig.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/PasswordEncoderConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class PasswordEncoderConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
