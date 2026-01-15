b12f2685cf3157b53ac0c04ce4734a65484acb93
Marika <mscrepnek2004@gmail.com>
2026-01-14 17:01:54 -0800

    create user repository

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.beautybuddy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.beautybuddy.model.User;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
