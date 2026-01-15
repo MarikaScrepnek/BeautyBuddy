4b906b2c9f5d57cc68aff1d487eca35817a575ad
Marika <mscrepnek2004@gmail.com>
2026-01-14 16:59:45 -0800

    create user entity

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/model/User.java
@@ -0,0 +1,42 @@
+package com.beautybuddy.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    private String username;
+    private String email;
+    private String passwordHash;
+
+    private LocalDateTime dateJoined = LocalDateTime.now();
+
+    public User() {}
+
+    public int getUser_id() { return user_id; }
+    public void setUser_id(int user_id) { this.user_id = user_id; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public LocalDateTime getDateJoined() { return dateJoined; }
+    public void setDateJoined(LocalDateTime dateJoined) { this.dateJoined = dateJoined; }
+}
