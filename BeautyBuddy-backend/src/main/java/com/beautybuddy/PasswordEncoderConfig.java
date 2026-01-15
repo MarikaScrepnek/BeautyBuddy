8778b6f78591483d54d73d20709177b5ba736d41
Marika <mscrepnek2004@gmail.com>
2026-01-14 17:39:25 -0800

    disable loging authentication for now (need access to the site)

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/PasswordEncoderConfig.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/PasswordEncoderConfig.java
@@ -2,11 +2,26 @@ package com.beautybuddy;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class PasswordEncoderConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Argon2PasswordEncoder(16, 32, 1, 65536, 3);
