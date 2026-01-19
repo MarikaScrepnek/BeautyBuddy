79d481036c7dd6d159044697603e38fb0eca011c
Marika <mscrepnek2004@gmail.com>
2026-01-19 10:29:05 -0800

    create ingridient and product_ingridient tables in postgresSQL

--- BeautyBuddy-backend/src/main/resources/schema.sql
+++ BeautyBuddy-backend/src/main/resources/schema.sql
@@ -22,6 +22,19 @@ CREATE TABLE IF NOT EXISTS product (
     CONSTRAINT unique_product_name_brand UNIQUE(name, brand_id)
 );
 
+CREATE TABLE IF NOT EXISTS ingredient (
+    ingredient_id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    normalized_name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product_ingredient (
+    product_id INT REFERENCES product(product_id),
+    ingredient_id INT REFERENCES ingredient(ingredient_id),
+    position INT,
+    PRIMARY KEY (product_id, ingredient_id)
+);
+
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
