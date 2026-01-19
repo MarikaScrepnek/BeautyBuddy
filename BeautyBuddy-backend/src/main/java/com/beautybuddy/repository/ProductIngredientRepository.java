4915838544176c0e3b130fb74ac8e40c9913ea63
Marika <mscrepnek2004@gmail.com>
2026-01-19 11:38:03 -0800

    update porductingredient repo

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ProductIngredientRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ProductIngredientRepository.java
@@ -1,5 +1,9 @@
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.beautybuddy.model.ProductIngredient;
 
+import com.beautybuddy.model.Product;
+import com.beautybuddy.model.Ingredient;
+
 public interface ProductIngredientRepository extends JpaRepository<ProductIngredient, Integer> {
+    boolean existsByProductAndIngredient(Product product, Ingredient ingredient);
 }
