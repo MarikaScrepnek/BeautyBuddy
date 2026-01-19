cac1ba8257b98e521946329dd238bd900f5a702e
Marika <mscrepnek2004@gmail.com>
2026-01-19 10:36:21 -0800

    create product_ingredient.java (join table)

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/model/ProductIngredient.java
@@ -0,0 +1,29 @@
+@Entity
+@Table (name = "product_ingredient"
+    UniqueConstraints = @UniqueConstraint(columnNames = {"product_id", "ingredient_id"})
+)
+public class ProductIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int product_ingredient_id;
+
+    @ManyToOne(nullable = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(nullable = false)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    public ProductIngredient() {}
+
+    public int getProduct_ingredient_id() { return product_ingredient_id; }
+    public void setProduct_ingredient_id(int product_ingredient_id) { this.product_ingredient_id = product_ingredient_id; }
+
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) { this.product = product; }
+
+    public Ingredient getIngredient() { return ingredient; }
+    public void setIngredient(Ingredient ingredient) { this.ingredient = ingredient; }
+}
