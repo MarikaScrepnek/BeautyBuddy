cb03dad7e56018d32a3d81d1cea103b12f177f54
Marika <mscrepnek2004@gmail.com>
2026-01-19 10:38:38 -0800

    create Ingredient repo

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/IngredientRepository.java
@@ -0,0 +1,8 @@
+package com.beautybuddy.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.beautybuddy.model.Ingredient;
+import java.util.Optional;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    Optional<Ingredient> findByNormalizedName(String normalizedName);
+}
