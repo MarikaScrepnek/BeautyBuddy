78d4bededcde2044b52ec966b810d48597b18beb
Marika <mscrepnek2004@gmail.com>
2026-01-20 11:50:55 -0800

    create ingredient json endpoint

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/controller/IngredientController.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/controller/IngredientController.java
@@ -3,23 +3,34 @@
 package com.beautybuddy.controller;
 
 import com.beautybuddy.service.ProductIngredientService;
+import com.beautybuddy.model.Ingredient;
 import com.beautybuddy.service.MayContainIngredientService;
+import com.beautybuddy.repository.IngredientRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ingredients")
 public class IngredientController {
 
+    private final IngredientRepository ingredientRepository;
+
     private final ProductIngredientService productIngredientService;
     private final MayContainIngredientService mayContainIngredientService;
 
-    public IngredientController(ProductIngredientService productIngredientService, MayContainIngredientService mayContainIngredientService) {
+    public IngredientController(IngredientRepository ingredientRepository, ProductIngredientService productIngredientService, MayContainIngredientService mayContainIngredientService) {
+        this.ingredientRepository = ingredientRepository;
         this.productIngredientService = productIngredientService;
         this.mayContainIngredientService = mayContainIngredientService;
+    }
+
+    @GetMapping
+    public List<Ingredient> getAllIngredients() {
+        return ingredientRepository.findAll();
     }
 
     @GetMapping("/parse")
