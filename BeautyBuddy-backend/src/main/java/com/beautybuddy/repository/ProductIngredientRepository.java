7497795c14fd7832a86149b31127b29a3f28642a
Marika <mscrepnek2004@gmail.com>
2026-01-20 11:56:45 -0800

    modify productingredientrepo to existbyproduct not both product and ingredient

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ProductIngredientRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ProductIngredientRepository.java
@@ -1,11 +1,10 @@
 package com.beautybuddy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.beautybuddy.model.ProductIngredient;
 
+import com.beautybuddy.model.ProductIngredient;
 import com.beautybuddy.model.Product;
-import com.beautybuddy.model.Ingredient;
 
 public interface ProductIngredientRepository extends JpaRepository<ProductIngredient, Integer> {
-    boolean existsByProductAndIngredient(Product product, Ingredient ingredient);
+    boolean existsByProduct(Product product);
 }
