0ec5a683beb9a5930cd9acb2300f90cafd8440f7
Marika <mscrepnek2004@gmail.com>
2026-01-20 12:06:18 -0800

    removed extraneous id field from productingredient entity that wasnt in sql schema

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/model/ProductIngredient.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/model/ProductIngredient.java
@@ -8,10 +8,6 @@ import jakarta.persistence.*;
 )
 public class ProductIngredient {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int product_ingredient_id;
-
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
@@ -24,9 +20,6 @@ public class ProductIngredient {
     private int position;
 
     public ProductIngredient() {}
-
-    public int getProduct_ingredient_id() { return product_ingredient_id; }
-    public void setProduct_ingredient_id(int product_ingredient_id) { this.product_ingredient_id = product_ingredient_id; }
 
     public Product getProduct() { return product; }
     public void setProduct(Product product) { this.product = product; }
