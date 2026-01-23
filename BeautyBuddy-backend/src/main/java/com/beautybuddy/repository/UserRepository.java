b2d79c7abb00b222c6975a6a24e9e1ca52225684
Marika <mscrepnek2004@gmail.com>
2026-01-23 11:53:00 -0800

    switch userrepo to search for email

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/UserRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/UserRepository.java
@@ -5,5 +5,5 @@ import com.beautybuddy.model.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
