6fc5032215279e37bcb993cfe19ac032e36cfc98
Marika <mscrepnek2004@gmail.com>
2026-01-27 13:26:22 -0800

    begin implementation of wishlistcontroller

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/controller/WishlistController.java
@@ -0,0 +1,37 @@
+package com.beautybuddy.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.beautybuddy.dto.AddToWishlistRequestDTO;
+import com.beautybuddy.dto.WishlistItemDTO;
+import com.beautybuddy.model.Wishlist;
+import com.beautybuddy.service.WishlistService;
+
+@RestController
+@RequestMapping("/api/wishlist")
+public class WishlistController {
+    private final WishlistService wishlistService;
+
+    public WishlistController(WishlistService wishlistService) {
+        this.wishlistService = wishlistService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Void> addToWishlist(@RequestBody AddToWishlistRequestDTO request) {
+        wishlistService.addToWishlist(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WishlistItemDTO>> getWishlist() {
+        List<WishlistItemDTO> wishlistItems = wishlistService.getWishlistItems();
+        return ResponseEntity.ok(wishlistItems);
+    }
+}
