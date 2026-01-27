a0724029c257bf805cf647496310485c45444dd8
Marika <mscrepnek2004@gmail.com>
2026-01-27 13:20:33 -0800

    make addtowishlistrequestdto a record

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/dto/AddToWishlistRequestDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/dto/AddToWishlistRequestDTO.java
@@ -1,22 +1,3 @@
 package com.beautybuddy.dto;
 
-public class AddToWishlistRequestDTO {
-    private int product_id;
-    private String shade_name;
-
-    public AddToWishlistRequestDTO() {}
-
-    public int getProduct_id() {
-        return product_id;
-    }
-    public void setProduct_id(int product_id) {
-        this.product_id = product_id;
-    }
-
-    public String getShade_name() {
-        return shade_name;
-    }
-    public void setShade_name(String shade_name) {
-        this.shade_name = shade_name;
-    }
-}
+public record AddToWishlistRequestDTO(int productId, String shadeName) {}
