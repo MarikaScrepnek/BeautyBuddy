33035accc24c3fac4c91dbff45f9c2a8e8a085e0
Marika <mscrepnek2004@gmail.com>
2026-01-27 14:18:17 -0800

    pass username from frontend in addtowishlist

--- BeautyBuddy-frontend/src/api/wishlistApi.js
+++ BeautyBuddy-frontend/src/api/wishlistApi.js
@@ -1,10 +1,11 @@
-export async function addToWishlist(productId, shadeId) {
+export async function addToWishlist(productId, shadeName) {
+    const username = localStorage.getItem("user");
     const res = await fetch("http://localhost:8080/api/wishlist/add", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ productId, shadeId }),
+        body: JSON.stringify({ username, productId, shadeName }),
         credentials: "include"
     });
     return res.ok;
