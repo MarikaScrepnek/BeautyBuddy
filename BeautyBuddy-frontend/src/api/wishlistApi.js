9c8ec6ebddc8e51803e7453a4f3a926e4f46093f
Marika <mscrepnek2004@gmail.com>
2026-01-28 14:10:42 -0800

    add removeFromWishlist and getWishlist in wishlistApi, and remove use of localstorage (cookies instead) in wishlistApi

--- BeautyBuddy-frontend/src/api/wishlistApi.js
+++ BeautyBuddy-frontend/src/api/wishlistApi.js
@@ -1,11 +1,34 @@
 export async function addToWishlist(productId, shadeName) {
-    const email = localStorage.getItem("user");
     const res = await fetch("http://localhost:8080/api/wishlist/add", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, productId, shadeName }),
+        credentials: "include",
+        body: JSON.stringify({ productId, shadeName }),
     });
     return res.ok;
+}
+
+export async function removeFromWishlist(productId, shadeName) {
+    const res = await fetch("http://localhost:8080/api/wishlist/remove", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ productId, shadeName }),
+    });
+    return res.ok;
+}
+
+export async function getWishlist() {
+    const res = await fetch("http://localhost:8080/api/wishlist", {
+        method: "GET",
+        credentials: "include",
+    });
+    if (!res.ok) {
+        throw new Error("Failed to fetch wishlist");
+    }
+    return res.json();
 }
