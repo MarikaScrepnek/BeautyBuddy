e6f0abfee2971550884177e202efb8989f004e0a
Marika <mscrepnek2004@gmail.com>
2026-01-29 15:13:33 -0800

    fix review repo errors

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ReviewRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/repository/ReviewRepository.java
@@ -1,15 +1,17 @@
 package com.beautybuddy.repository;
 
 import com.beautybuddy.model.Review;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-    Optional<Review> findByProductId(int productId);
 
-    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.product.productId = :productId AND r.approved = true")
-    Optional<Double> findAverageRatingByProductId(int productId);
+    @Query("SELECT r FROM Review r WHERE r.product.product_id = :productId AND r.deletedAt IS NULL AND r.approved = true")
+    Page<Review> findByProductId(@Param("productId") int productId, Pageable pageable);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.product.product_id = :productId AND r.deletedAt IS NULL AND r.approved = true")
+    Double findAverageRatingByProductId(@Param("productId") int productId);
 }
