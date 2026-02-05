2d9a719b51ea291b5783645e4f5de399ab9f639a
Marika <mscrepnek2004@gmail.com>
2026-02-05 12:19:21 -0800

    edit review DTO to only hold important fields

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewDTO.java
@@ -1,26 +1,13 @@
 package com.beautybuddy.review;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.beautybuddy.product.ProductDTO;
-import com.beautybuddy.product.ProductShadeDTO;
-import com.beautybuddy.user.UserDTO;
+import java.math.BigDecimal;
 
 public record ReviewDTO(
-    int reviewId,
-    UserDTO user,
-    ProductDTO product,
-    ProductShadeDTO productShade,
-    float rating,
+    int productId,
+    String shadeName,
+    BigDecimal rating,
     String reviewText,
-    int helpfulCount,
-    int reportedCount,
-    boolean approved,
-    LocalDateTime createdAt,
-    LocalDateTime updatedAt,
-    LocalDateTime deletedAt,
-    List<ReviewImageDTO> reviewImages
+    String imageLink
 )
 { 
 }
