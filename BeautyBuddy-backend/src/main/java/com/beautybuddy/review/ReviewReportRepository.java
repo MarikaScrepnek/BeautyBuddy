7e3b092f71d9002206b27a2058c2a220c5c848e8
Marika <mscrepnek2004@gmail.com>
2026-02-05 15:12:01 -0800

    create reviewreportrepo

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewReportRepository.java
@@ -0,0 +1,6 @@
+package com.beautybuddy.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewReportRepository extends JpaRepository<ReviewReport, Integer> {
+}
