97682639f9807546cb4756f881b74e96f3f4d9b4
Marika <mscrepnek2004@gmail.com>
2026-02-05 15:11:05 -0800

    create entity for reviewreport

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewReport.java
@@ -0,0 +1,96 @@
+package com.beautybuddy.review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+import java.time.LocalDateTime;
+
+import com.beautybuddy.user.User;
+
+@Entity
+@Table(
+    name = "review_report",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"account_id", "review_id"})
+)
+public class ReviewReport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id", nullable = false)
+    private int reviewReportId;
+
+    @Column(name = "account_id", nullable = false)
+    private User user;
+
+    @Column(name = "review_id", nullable = false)
+    private Review review;
+
+    @Column(name = "reason", nullable = true)
+    private String reason;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "resolved_at", nullable = true)
+    private LocalDateTime resolvedAt;
+
+    public ReviewReport() {
+    }
+
+    public int getReviewReportId() {
+        return reviewReportId;
+    }
+    public void setReviewReportId(int reviewReportId) {
+        this.reviewReportId = reviewReportId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+    public void setReview(Review review) {
+        this.review = review;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getResolvedAt() {
+        return resolvedAt;
+    }
+    public void setResolvedAt(LocalDateTime resolvedAt) {
+        this.resolvedAt = resolvedAt;
+    }
+
+}
