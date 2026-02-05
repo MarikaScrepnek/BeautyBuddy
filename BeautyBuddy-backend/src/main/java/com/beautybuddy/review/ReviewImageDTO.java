e588e7f0f3d210f3a83720a4fe1f0488254ae656
Marika <mscrepnek2004@gmail.com>
2026-02-05 13:22:12 -0800

    commented in last commit

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewImageDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/review/ReviewImageDTO.java
@@ -1,11 +1,7 @@
 package com.beautybuddy.review;
 
-import java.time.LocalDateTime;
-
 public record ReviewImageDTO(
-    int reviewImageId,
     ReviewDTO review,
-    String imageLink,
-    LocalDateTime uploadedAt
+    String imageLink
 ) {
 }
