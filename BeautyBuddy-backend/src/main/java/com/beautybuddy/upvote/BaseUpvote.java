054d44fe0448bdd2db1f60434e554de4a3246e6e
Marika <mscrepnek2004@gmail.com>
2026-02-06 11:57:56 -0800

    create baseupvote mapped superclass, create upvote folder

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/BaseUpvote.java
@@ -0,0 +1,34 @@
+package com.beautybuddy.upvote;
+
+import com.beautybuddy.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@MappedSuperclass
+public abstract class BaseUpvote {
+
+	@ManyToOne
+	@JoinColumn(name = "account_id", nullable = false)
+	private User user;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+}
