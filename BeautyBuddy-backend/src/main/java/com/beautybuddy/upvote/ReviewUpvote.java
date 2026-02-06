ccaad56c679e9ae0a1ea61fb320fb84d80e4e032
Marika <mscrepnek2004@gmail.com>
2026-02-06 11:59:36 -0800

    change reviewupvote to extend baseupvote, move it to upvote folder

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/ReviewUpvote.java
@@ -0,0 +1,43 @@
+package com.beautybuddy.upvote;
+
+import com.beautybuddy.review.Review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+    name = "review_upvotes",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"account_id", "review_id"})
+)
+public class ReviewUpvote extends BaseUpvote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_upvote_id")
+    private Integer reviewUpvoteId;
+
+    @ManyToOne
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    public Integer getReviewUpvoteId() {
+        return reviewUpvoteId;
+    }
+    public void setReviewUpvoteId(Integer reviewUpvoteId) {
+        this.reviewUpvoteId = reviewUpvoteId;
+    }
+
+    public Review getReview() {
+        return review;
+    }
+    public void setReview(Review review) {
+        this.review = review;
+    }
+}
