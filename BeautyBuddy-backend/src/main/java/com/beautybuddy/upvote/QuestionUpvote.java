5f2bf1705ffb51c984fe821c089724ad2e6ffc72
Marika <mscrepnek2004@gmail.com>
2026-02-06 12:20:21 -0800

    make QuestionUpvote that extends BaseUpvote

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/QuestionUpvote.java
@@ -0,0 +1,41 @@
+package com.beautybuddy.upvote;
+
+import com.beautybuddy.q&a.Question;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Table(
+    name = "question_upvotes",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"account_id", "question_id"})
+)
+public class QuestionUpvote extends BaseUpvote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_upvote_id")
+    private Integer questionUpvoteId;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    public Integer getQuestionUpvoteId() {
+        return questionUpvoteId;
+    }
+    public void setQuestionUpvoteId(Integer questionUpvoteId) {
+        this.questionUpvoteId = questionUpvoteId;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
