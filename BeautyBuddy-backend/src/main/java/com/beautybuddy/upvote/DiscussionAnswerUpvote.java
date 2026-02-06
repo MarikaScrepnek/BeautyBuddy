4ba4975c24e1ec65e9f2750b861e97727df1d0b7
Marika <mscrepnek2004@gmail.com>
2026-02-06 12:26:48 -0800

    make DiscussionAnswerUpvote that extends BaseUpvote

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/DiscussionAnswerUpvote.java
@@ -0,0 +1,42 @@
+package com.beautybuddy.upvote;
+
+import com.beautybuddy.discussion.DiscussionAnswer;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.UniqueConstraint;
+
+@Table(
+    name = "discussion_answer_upvotes",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"account_id", "discussion_answer_id"})
+)
+public class DiscussionAnswerUpvote extends BaseUpvote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "discussion_answer_upvote_id")
+    private Integer discussionAnswerUpvoteId;
+
+    @ManyToOne
+    @JoinColumn(name = "discussion_answer_id", nullable = false)
+    private DiscussionAnswer discussionAnswer;
+
+    public Integer getDiscussionAnswerUpvoteId() {
+        return discussionAnswerUpvoteId;
+    }
+    public void setDiscussionAnswerUpvoteId(Integer discussionAnswerUpvoteId) {
+        this.discussionAnswerUpvoteId = discussionAnswerUpvoteId;
+    }
+
+    public DiscussionAnswer getDiscussionAnswer() {
+        return discussionAnswer;
+    }
+    public void setDiscussionAnswer(DiscussionAnswer discussionAnswer) {
+        this.discussionAnswer = discussionAnswer;
+    }
+}
