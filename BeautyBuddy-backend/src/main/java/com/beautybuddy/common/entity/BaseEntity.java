cad0fb8c69e249c8872aee3f15be8b16e6226b06
Marika <mscrepnek2004@gmail.com>
2026-02-10 16:57:58 -0800

    implement baseentity, add comments saying what classes will extend it

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/common/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.beautybuddy.common.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
+// product_ingredient, may_contain_ingredient, account_follow, wishlist_item, routine_item, user_discussion_pin, notification, activity, product_purchase
+
+// none (not even base - all have FPKs): account_notification_preference, wishlist, notification types, activity types
