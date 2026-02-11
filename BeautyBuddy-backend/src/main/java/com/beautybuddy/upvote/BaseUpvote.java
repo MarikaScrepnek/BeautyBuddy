9813064915a4f8a9e3df7c8580d10076d2341ea7
Marika <mscrepnek2004@gmail.com>
2026-02-10 16:57:01 -0800

    modify baseupvote to extend baseentity

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/BaseUpvote.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/upvote/BaseUpvote.java
@@ -1,39 +1,24 @@
 package com.beautybuddy.upvote;
 
+import com.beautybuddy.common.entity.BaseEntity;
 import com.beautybuddy.user.User;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public abstract class BaseUpvote {
+public abstract class BaseUpvote extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false, updatable = false)
+    private User user;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private int id;
-
-	@ManyToOne
-	@JoinColumn(name = "account_id", nullable = false)
-	private User user;
-
-	@Column(name = "created_at", nullable = false, insertable = false, updatable = false)
-	private LocalDateTime createdAt;
-
-	public User getUser() {
-		return user;
-	}
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
+
+// upvotes
