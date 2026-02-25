04ed16bdc29752400dbb9d39c5d603dac56b2284
Marika <mscrepnek2004@gmail.com>
2026-02-25 11:09:28 -0800

    add edit api calls to discussion

--- BeautyBuddy-frontend/src/api/discussionApi.js
+++ BeautyBuddy-frontend/src/api/discussionApi.js
@@ -22,12 +22,32 @@ export async function createDiscussion( title, text ) {
     return response.ok;
 }
 
+export async function editDiscussion(discussionId, title, text) {
+    const response = await fetch(`http://localhost:8080/api/discussions/${discussionId}/edit`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ title, text }),
+    });
+    return response.ok;
+}
+
 export async function createComment(discussionId, parentDiscussionCommentId, text) {
     const response = await fetch(`http://localhost:8080/api/discussions/${discussionId}/comment`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
         body: JSON.stringify({ parentDiscussionCommentId, text }),
+    });
+    return response.ok;
+}
+
+export async function editComment(commentId, text) {
+    const response = await fetch(`http://localhost:8080/api/discussions/comments/${commentId}/edit`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify({ text }),
     });
     return response.ok;
 }
