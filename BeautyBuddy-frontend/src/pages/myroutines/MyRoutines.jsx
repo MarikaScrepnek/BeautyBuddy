285c680e0018d755e3c360643b434e4c5e3b0107
Marika <mscrepnek2004@gmail.com>
2026-02-26 14:17:41 -0800

    made card and container for my routines page

--- BeautyBuddy-frontend/src/pages/myroutines/MyRoutines.jsx
+++ BeautyBuddy-frontend/src/pages/myroutines/MyRoutines.jsx
@@ -4,6 +4,8 @@ import { getCurrentUser } from '../../api/authApi';
 
 import Wishlist from './components/Wishlist';
 
+import "./MyRoutines.css";
+
 export default function MyRoutines() {
   // log in
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,18 +23,22 @@ export default function MyRoutines() {
   }, []);
 
   return (
-  <>
-    <div>
-      {isLoggedIn ? <p>Welcome back, {username}!</p> : <p>Please log in to view your routines.</p>}
-    </div>
+  <div className='routines-page-container'>
+    <div className='routines-page-card'>
 
-    <div className='wishlist-container'>
-        <Wishlist isLoggedIn={isLoggedIn} />
-    </div>
+      <div style={{textAlign: "center", textDecoration: "underline", textDecorationColor: "#f0cef0" }}>
+        {isLoggedIn ? <p>Welcome back, {username}!</p> : <p>Please log in to view your routines.</p>}
+      </div>
 
-    <div className='routine-container'>
-      <h1>Routines</h1>
+      <div className='wishlist-container'>
+          <Wishlist isLoggedIn={isLoggedIn} />
+      </div>
+
+      <div className='routine-container'>
+        <h1>Routines</h1>
+      </div>
+
     </div>
-  </>
+  </div>
   );
 }
