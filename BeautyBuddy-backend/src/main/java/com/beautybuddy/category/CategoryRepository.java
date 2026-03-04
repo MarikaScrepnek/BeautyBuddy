b3d19cf2d9dafc4b1138a94d06bffb8cb7ff76de
Marika <mscrepnek2004@gmail.com>
2026-03-03 17:23:06 -0800

    add findbyname to categoryrepo

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/category/CategoryRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/category/CategoryRepository.java
@@ -1,6 +1,9 @@
 package com.beautybuddy.category;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findByName(String name);
 }
