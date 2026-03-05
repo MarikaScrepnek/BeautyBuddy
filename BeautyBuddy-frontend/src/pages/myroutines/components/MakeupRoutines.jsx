fcb6848d8e1924f0278353087392c39001e4eec4
Marika <mscrepnek2004@gmail.com>
2026-03-05 15:52:26 -0800

    receive routine as arg

--- BeautyBuddy-frontend/src/pages/myroutines/components/MakeupRoutines.jsx
+++ BeautyBuddy-frontend/src/pages/myroutines/components/MakeupRoutines.jsx
@@ -5,53 +5,32 @@ import CreateRoutineModal from './CreateRoutineModal';
 
 import './MakeupRoutines.css';
 
-export default function MakeupRoutines() {
+export default function MakeupRoutines( { userName, routine } ) {
     const[createModalOpen, setCreateModalOpen] = useState(false);
-    const[makeupRoutines, setMakeupRoutines] = useState([]);
-
-    useEffect(() => {
-        getMakeupRoutines()
-            .then(data => {
-                setMakeupRoutines(data);
-            })
-            .catch(error => {
-                console.error("Error fetching makeup routines:", error);
-            });
-    }, []);
 
     return (
         <div className="routine-card">
             <div className="makeup-routine-header">
-                <h1>Makeup Routines</h1>
-                <button
-                className="create-button"
-                onClick={() => setCreateModalOpen(true)}
-                >
-                    +
-                </button>
+                <h1> {routine.name || routine.occasion} </h1>
+                <p>Created by: {userName}</p>
             </div>
             
-            {makeupRoutines.map(routine => (
-                <div key={routine.routineId} className="routine-item">
 
-                    <h3>{routine.name || routine.occasion}</h3>
-                    {routine.items && routine.items.length > 0 ? (
-                        <ul>
-                            {routine.items.map(item => (
-                                <div className="routine-item" key={item.productId}>
-                                    <p>{item.productName}</p>
-                                    <p>{item.productShadeName}</p>
-                                    <p>{item.productBrand}</p>
-                                    <p>{item.category}</p>
-                                    <img src={item.productImageUrl} alt={item.productName} style={{ width: '50px', height: '50px' }} />
-                                </div>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>No items in this routine.</p>
-                    )}
-                </div>
-            ))}
+            {routine.items && routine.items.length > 0 ? (
+                <ul>
+                    {routine.items.map(item => (
+                        <div className="routine-item" key={item.id}>
+                            <p>{item.productName}</p>
+                            <p>{item.productShadeName}</p>
+                            <p>{item.productBrand}</p>
+                            <p>{item.category}</p>
+                            <img src={item.productImageUrl} alt={item.productName} style={{ width: '50px', height: '50px' }} />
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <p>No items in this routine.</p>
+            )}
 
 
             {createModalOpen && <CreateRoutineModal onClose={() => setCreateModalOpen(false)} />}
