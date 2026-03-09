38188eb397030480ee65b89281451796b740a805
Marika <mscrepnek2004@gmail.com>
2026-03-09 15:43:41 -0700

    accomodate last commit

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/routine/dto/CreateMakeupRoutineRequestDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/routine/dto/CreateMakeupRoutineRequestDTO.java
@@ -1,6 +1,6 @@
 package com.beautybuddy.routine.dto;
 
-import com.beautybuddy.routine.OccasionEnum;
+import com.beautybuddy.routine.entity.OccasionEnum;
 
 public record CreateMakeupRoutineRequestDTO(
     OccasionEnum occasion,
