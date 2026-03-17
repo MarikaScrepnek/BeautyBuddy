37bddae5c3fb1893ebfcdfe400b914f0aaf36d59
Marika <mscrepnek2004@gmail.com>
2026-03-17 16:50:38 -0700

    create base repos for breakout list entities

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListProductRepository.java
@@ -0,0 +1,5 @@
+package com.beautybuddy.breakout.repo;
+
+public interface BreakoutListProductRepository extends org.springframework.data.jpa.repository.JpaRepository<com.beautybuddy.breakout.entity.BreakoutListProduct, Long> {
+    
+}
---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListRepository.java
@@ -0,0 +1,5 @@
+package com.beautybuddy.breakout.repo;
+
+public interface BreakoutListRepository extends org.springframework.data.jpa.repository.JpaRepository<com.beautybuddy.breakout.entity.BreakoutList, Long> {
+    
+}
