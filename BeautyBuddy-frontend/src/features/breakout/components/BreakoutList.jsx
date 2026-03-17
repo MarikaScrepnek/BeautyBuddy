50b886d881d83ac932cfe812db4bbb895bc847bd
Marika <mscrepnek2004@gmail.com>
2026-03-17 16:53:08 -0700

    create breakoutlist jsx and css files

--- BeautyBuddy-frontend/src/features/breakout/components/BreakoutList.jsx
+++ BeautyBuddy-frontend/src/features/breakout/components/BreakoutList.jsx
@@ -1,8 +1,34 @@
+import "./BreakoutList.css";
+
+import { useState } from "react";
+
 export default function BreakoutList() {
-  return (
-    <div className="breakout-list">
-      <h2>Breakout List</h2>
-      <p>This is where the breakout list will be displayed.</p>
-    </div>
-  );
+    const [ingredientsSelected, setIngredientsSelected] = useState(false);
+    const [productsSelected, setProductsSelected] = useState(true);
+
+    return (
+    <div className="card-container">
+        <div className="card-header">
+            <h2>Breakout List</h2>
+            <div className="card-buttons">
+                <button 
+                    className={ingredientsSelected ? "selected" : ""}
+                    onClick={() => {setIngredientsSelected(true); setProductsSelected(false)}}>
+                    Ingredients
+                </button>
+                <button 
+                    className={productsSelected ? "selected" : ""}
+                    onClick={() => {setProductsSelected(true); setIngredientsSelected(false)}}>
+                    Products
+                </button>
+            </div>
+        </div>
+        <div className="card-body">
+
+            {ingredientsSelected && <p>Ingredients breakout list will be displayed here.</p>}
+
+            {productsSelected && <p>Products breakout list will be displayed here.</p>}
+        </div>
+    </div> 
+    );
 }
