35c3589b003eb5872bdf0778988c3860b175f73a
Marika <mscrepnek2004@gmail.com>
2026-03-18 14:24:04 -0700

    add findbybreakoutlistidandingredientid to breakoutitem repos

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListIngredientRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListIngredientRepository.java
@@ -1,5 +1,5 @@
 package com.beautybuddy.breakout.repo;
 
 public interface BreakoutListIngredientRepository extends org.springframework.data.jpa.repository.JpaRepository<com.beautybuddy.breakout.entity.BreakoutListIngredient, Long> {
-    
+    java.util.Optional<com.beautybuddy.breakout.entity.BreakoutListIngredient> findByBreakoutListIdAndIngredientId(Long breakoutListId, Long ingredientId);
 }
--- BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListProductRepository.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/repo/BreakoutListProductRepository.java
@@ -1,5 +1,5 @@
 package com.beautybuddy.breakout.repo;
 
 public interface BreakoutListProductRepository extends org.springframework.data.jpa.repository.JpaRepository<com.beautybuddy.breakout.entity.BreakoutListProduct, Long> {
-    
+    java.util.Optional<com.beautybuddy.breakout.entity.BreakoutListProduct> findByBreakoutListIdAndProductId(Long breakoutListId, Long productId);
 }
