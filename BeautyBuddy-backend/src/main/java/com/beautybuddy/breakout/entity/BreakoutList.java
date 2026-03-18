704ee12e694acfab513a4bc9429257e103671866
Marika <mscrepnek2004@gmail.com>
2026-03-18 14:23:27 -0700

    add getid and setid methods to breakoutlist

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/entity/BreakoutList.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/breakout/entity/BreakoutList.java
@@ -83,4 +83,11 @@ public class BreakoutList extends ForeignKeyIdEntity{
     protected Long getForeignKeyId() {
         return user.getId();
     }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
