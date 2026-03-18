b12859fe019f3e39caa8e8e21b9bf740cbfcef2c
Marika <mscrepnek2004@gmail.com>
2026-03-18 14:26:02 -0700

    add removefrombreakoutlist and getbreakoutlistproducts api calls (js)

--- BeautyBuddy-frontend/src/features/breakout/api/breakoutListApi.js
+++ BeautyBuddy-frontend/src/features/breakout/api/breakoutListApi.js
@@ -8,7 +8,7 @@ export async function addToBreakoutList(type, id) {
         },
         body: JSON.stringify({ ingredientId: id, productId: null })
     });
-    return response.json();
+    return response.ok;
     }
     else if (type === "product") {
         const response = await fetch(`http://localhost:8080/api/breakout-list/add`, {
@@ -19,6 +19,37 @@ export async function addToBreakoutList(type, id) {
         },
         body: JSON.stringify({ productId: id, ingredientId: null })
     });
+    return response.ok;
+    }
+}
+
+export async function getBreakoutListProducts() {
+    const response = await fetch(`http://localhost:8080/api/breakout-list`, {
+        method: "GET",
+        credentials: "include"
+    });
     return response.json();
+}
+
+export async function removeFromBreakoutList(type, id) {
+    if (type === "ingredient") {
+        const response = await fetch(`http://localhost:8080/api/breakout-list/remove`, {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"        },
+        body: JSON.stringify({ ingredientId: id, productId: null })
+    });
+    return response.ok;
+    }
+    else if (type === "product") {
+        const response = await fetch(`http://localhost:8080/api/breakout-list/remove`, {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"        },
+        body: JSON.stringify({ productId: id, ingredientId: null })
+    });
+    return response.ok;
     }
 }
