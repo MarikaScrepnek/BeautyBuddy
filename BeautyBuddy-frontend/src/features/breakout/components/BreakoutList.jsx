5eb8b035a5ece2a1d6a7f9145a48a2d1fae2c610
Marika <mscrepnek2004@gmail.com>
2026-03-18 14:26:57 -0700

    create base breakoutlist product card

--- BeautyBuddy-frontend/src/features/breakout/components/BreakoutList.jsx
+++ BeautyBuddy-frontend/src/features/breakout/components/BreakoutList.jsx
@@ -1,10 +1,35 @@
+import { getBreakoutListProducts, removeFromBreakoutList } from "../api/breakoutListApi";
+import { GiTrashCan } from "react-icons/gi";
 import "./BreakoutList.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 
 export default function BreakoutList() {
-    const [ingredientsSelected, setIngredientsSelected] = useState(false);
-    const [productsSelected, setProductsSelected] = useState(true);
+    const [ingredientsSelected, setIngredientsSelected] = useState(true);
+    const [productsSelected, setProductsSelected] = useState(false);
+    const [breakoutListProducts, setBreakoutListProducts] = useState([]);
+
+    useEffect(() => {
+        getBreakoutListProducts()
+            .then(data => {
+                console.log("Breakout List:", data);
+                setBreakoutListProducts(data);
+            })
+            .catch(error => {
+                console.error("Error fetching breakout list:", error);
+            });
+    }, []);
+
+    async function handleDeleteProduct(productId) {
+        removeFromBreakoutList("product", productId)
+            .then(() => {
+                setBreakoutListProducts(prevProducts => prevProducts.filter(product => product.productId !== productId));
+            })
+            .catch(error => {
+                console.error("Error removing item from breakout list:", error);
+            });
+    }
 
     return (
     <div className="card-container">
@@ -25,9 +50,49 @@ export default function BreakoutList() {
         </div>
         <div className="card-body">
 
-            {ingredientsSelected && <p>Ingredients breakout list will be displayed here.</p>}
+            {ingredientsSelected && (
+                <p>Ingredients will be displayed here in the future.</p>
+            )}
 
-            {productsSelected && <p>Products breakout list will be displayed here.</p>}
+            {productsSelected && (
+                <ul>
+                    {breakoutListProducts.map((product, index) => (
+                        <React.Fragment key={product.id}>
+                            <li
+                            className="routine-item"
+                            onClick={() => {
+                                window.open(`/products/${product.productId}`, '_blank')
+                            }}
+                        >
+                            <div className="delete-item-button" onClick={(e) => {e.stopPropagation(); handleDeleteProduct(product.productId);}}>
+                                <GiTrashCan />
+                            </div>
+                            <img draggable="false" src={product.productImageUrl} alt={product.productName} className="routine-item-img" />
+                            <div className="routine-item-main">
+                                <div className='routine-item-info-row'>
+                                    <div className="routine-item-info">
+                                        <p className="routine-item-name">{product.productName}</p>
+                                        <div className='routine-item-meta'>
+                                            <span>{product.productBrand}</span>
+                                        </div>
+                                        {product.ingredients.length > 0 && (
+                                            <div className="routine-item-ingredients">
+                                                {product.ingredients.map(ingredient => (
+                                                    <span key={ingredient.name} className="routine-item-ingredient">
+                                                        {ingredient.name}
+                                                        {index < product.ingredients.length - 1 && " • "}
+                                                    </span>
+                                                ))}
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        </React.Fragment>
+                    ))}
+                </ul>
+            )}
         </div>
     </div> 
     );
