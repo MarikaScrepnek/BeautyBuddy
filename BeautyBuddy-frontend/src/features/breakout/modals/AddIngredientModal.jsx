51537548b26973b3ee28c6f3907facc435b03c6a
Marika <mscrepnek2004@gmail.com>
2026-03-20 12:51:46 -0700

    add product list and add and search front end to ingredient modal

--- BeautyBuddy-frontend/src/features/breakout/modals/AddIngredientModal.jsx
+++ BeautyBuddy-frontend/src/features/breakout/modals/AddIngredientModal.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import getIngredients from "../api/ingredientApi";
 
+import "./AddIngredientModal.css";
+import Tooltip from "../../../components/ui/Tooltip";
+import Searchbar from "../../../components/ui/Searchbar";
+
 export default function AddIngredientModal({ onClose }) {
+    const [ingredients, setIngredients] = useState([]);
 
     useEffect(() => {
-        getIngredients()
+        getIngredients(0, 8)
             .then(data => {
-                console.log("Ingredients:", data);
+                setIngredients(data.content);
             })
             .catch(error => {
                 console.error("Error fetching ingredients:", error);
@@ -16,9 +21,24 @@ export default function AddIngredientModal({ onClose }) {
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <h2>Add Ingredient</h2>
-                <p>This feature is coming soon! In the meantime, you can add products to your breakout list and view their ingredients.</p>
-                <button onClick={onClose}>Close</button>
+                <button className="modal-close" onClick={onClose}>X</button>
+
+                <h2>Add Ingredient to Breakout List</h2>
+
+                <Searchbar placeholder="Search ingredients..." onSearch={query => console.log("Search for:", query)} />
+
+                {ingredients.length > 0 && (
+                    <ul>
+                        {ingredients.map(ingredient => (
+                            <div className="ingredient-card" key={ingredient.id}>
+                                <p>{ingredient.name}</p>
+                                <Tooltip message={`Add to breakout list`}>
+                                    <button>+</button>
+                                </Tooltip>
+                            </div>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     );
