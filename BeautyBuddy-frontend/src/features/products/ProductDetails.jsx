4e8e2f8956e01bb011b1ace9334f8ff9cf1f3d12
Marika <mscrepnek2004@gmail.com>
2026-03-25 10:39:51 -0700

    fix name error in call to addtoroutinemodal

--- BeautyBuddy-frontend/src/features/products/ProductDetails.jsx
+++ BeautyBuddy-frontend/src/features/products/ProductDetails.jsx
@@ -997,7 +997,7 @@ export default function ProductDetails() {
         </div>
         {addToRoutineOpen && (
             <AddToRoutineModal
-                baseCategory={data.category.baseCategoryName}
+                baseCategoryName={data.category.baseCategoryName}
                 productName={data.name}
                 productId={data.id}
                 shadeName={selectedShade?.shadeName ?? ""}
