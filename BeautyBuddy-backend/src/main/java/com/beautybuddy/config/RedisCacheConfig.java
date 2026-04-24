bf64eee937762b0e96a008f00d0ebfaa3a3648aa
Marika <mscrepnek2004@gmail.com>
2026-04-24 16:02:49 -0700

    allow redis on integration tests

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/config/RedisCacheConfig.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/config/RedisCacheConfig.java
@@ -9,7 +9,6 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -21,7 +20,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
-@Profile("!test")
 public class RedisCacheConfig {
 
     private static final Logger log = LoggerFactory.getLogger(RedisCacheConfig.class);
