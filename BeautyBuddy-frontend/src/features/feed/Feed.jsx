0890ab5cf9b260f991276bb918c916b59dd70e1a
Marika <mscrepnek2004@gmail.com>
2026-04-24 17:52:41 -0700

    simplify feed uninmenplemted page

--- BeautyBuddy-frontend/src/features/feed/Feed.jsx
+++ BeautyBuddy-frontend/src/features/feed/Feed.jsx
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { getCurrentUser } from '../../features/auth/api/authApi';
-
 export default function Feed() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        getCurrentUser()
-            .then(() => setIsLoggedIn(true))
-            .catch(() => setIsLoggedIn(false));
-    }, []);
     return (
-        <>
-        {!isLoggedIn ? (
 
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
-                <h1 style={{fontSize: '2rem', color: '#555'}}>Login to view your feed...</h1>
-            </div>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
+            <h1 style={{fontSize: '2rem', color: '#555'}}>Feed coming soon...</h1>
+        </div>
 
-        ) : (
-
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh'}}>
-                <h1 style={{fontSize: '2rem', color: '#555'}}>Feed coming soon...</h1>
-            </div>
-
-        )}
-        </>
     );
 }
