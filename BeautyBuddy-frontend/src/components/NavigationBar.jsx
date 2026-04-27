cf35980b5e1d1c64d560761955c13a0e28fad571
Marika <mscrepnek2004@gmail.com>
2026-04-27 14:04:05 -0700

    fix inconsistency with nav bar when logged in vs  not (profile vs routines)

--- BeautyBuddy-frontend/src/components/NavigationBar.jsx
+++ BeautyBuddy-frontend/src/components/NavigationBar.jsx
@@ -58,8 +58,8 @@ export default function NavigationBar() {
             <NavLink to="/discussions" className={({ isActive }) => isActive ? 'active-link' : undefined}>Discussions</NavLink>
             {!isLoggedIn ? (
               <>
-                <Tooltip message="Please log in to view your routines" position="bottom">
-                  <p className="not-logged-in-links">Routines</p>
+                <Tooltip message="Please log in to view your profile" position="bottom">
+                  <p className="not-logged-in-links">Profile</p>
                 </Tooltip>
                 <Tooltip message={"Please log in to view your feed"} position="bottom">
                   <p className="not-logged-in-links">Feed</p>
