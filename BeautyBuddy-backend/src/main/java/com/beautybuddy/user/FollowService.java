e88badfa0786505e25d561169c02329d0ac01907
Marika <mscrepnek2004@gmail.com>
2026-05-28 12:17:29 -0700

    service for getting followers and following

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/user/FollowService.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/user/FollowService.java
@@ -1,7 +1,10 @@
 package com.beautybuddy.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.beautybuddy.user.dto.UserSearchDTO;
 import com.beautybuddy.user.entity.User;
 import com.beautybuddy.user.entity.UserFollow;
 import com.beautybuddy.user.repo.FollowRepository;
@@ -57,11 +60,13 @@ public class FollowService {
         followRepo.delete(follow);
     }
 
-    /* public List<UserSearchDTO> getFollowers(String username) {
-        // Implement logic to get followers here
+    public Page<UserSearchDTO> getFollowers(String username) {
+        Page<UserFollow> followers = followRepo.findByFollowedUsername(username, PageRequest.of(0, 20));
+        return followers.map(follow -> new UserSearchDTO(follow.getFollower().getUsername(), follow.getFollower().getUsername()));
     }
 
-    public List<UserSearchDTO> getFollowing(String username) {
-        // Implement logic to get following here
-    } */
+    public Page<UserSearchDTO> getFollowing(String username) {
+        Page<UserFollow> following = followRepo.findByFollowerUsername(username, PageRequest.of(0, 20));
+        return following.map(follow -> new UserSearchDTO(follow.getFollowed().getUsername(), follow.getFollowed().getUsername()));
+    }
 }
