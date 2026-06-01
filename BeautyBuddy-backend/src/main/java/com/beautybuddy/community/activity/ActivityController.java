22afbae481682113d8f1f4d648cfca41687fd616
Marika <mscrepnek2004@gmail.com>
2026-06-01 16:28:42 -0700

    remove post endpoint for activities, should be server only

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityController.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityController.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +15,6 @@ public class ActivityController {
 
     public ActivityController(ActivityService activityService) {
         this.activityService = activityService;
-    }
-
-    @PostMapping
-    public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
-        Activity createdActivity = activityService.createActivity(activity);
-        return ResponseEntity.ok(createdActivity);
     }
 
     @GetMapping("/{username}")
