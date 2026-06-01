f443e74dbb746d4398e552c0827e30e59c09208f
Marika <mscrepnek2004@gmail.com>
2026-06-01 16:25:32 -0700

    activity controller skeleton

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityController.java
@@ -0,0 +1,32 @@
+package com.beautybuddy.community.activity;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/activities")
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
+        Activity createdActivity = activityService.createActivity(activity);
+        return ResponseEntity.ok(createdActivity);
+    }
+
+    @GetMapping("/{username}")
+    public Page<ResponseEntity<Activity>> getActivities(@PathVariable String username) {
+        return activityService.getActivitiesByUsername(username);
+    }
+}
