82d4ff0310d932240c3054576c8ba3823103768b
Marika <mscrepnek2004@gmail.com>
2026-06-02 18:47:56 -0700

    add get following activities get endpoint

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityController.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityController.java
@@ -2,10 +2,14 @@ package com.beautybuddy.community.activity;
 
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beautybuddy.community.activity.entity.Activity;
+import com.beautybuddy.security.CustomUserDetails;
 
 @RestController
 @RequestMapping("/api/activities")
@@ -20,5 +24,15 @@ public class ActivityController {
     @GetMapping("/{username}")
     public Page<ResponseEntity<Activity>> getActivities(@PathVariable String username) {
         return activityService.getActivitiesByUsername(username);
+    }
+
+    @GetMapping("following")
+    public Page<ResponseEntity<Activity>> getFollowingActivities(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Page.empty();
+        }
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        return activityService.getFollowingActivitiesByUsername(userDetails.getUsername());
     }
 }
