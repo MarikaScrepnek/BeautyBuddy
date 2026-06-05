61791f2973ad7f0a9bc3fb6ef4993ea7d711cf09
Marika <mscrepnek2004@gmail.com>
2026-06-04 20:25:57 -0700

    add target_id to activity table

--- BeautyBuddy-backend/src/main/resources/db/migration/V6__feed_page_modifications.sql
+++ BeautyBuddy-backend/src/main/resources/db/migration/V6__feed_page_modifications.sql
@@ -5,3 +5,9 @@ DROP TABLE routine_item_activity;
 DROP TABLE routine_image_activity;
 
 DROP TABLE wishlist_item_activity;
+
+ALTER TABLE activity
+    ADD COLUMN target_id BIGINT;
+
+ALTER TABLE activity
+    ALTER COLUMN target_id SET NOT NULL;
