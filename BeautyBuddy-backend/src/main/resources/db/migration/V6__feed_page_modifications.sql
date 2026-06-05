bb85bd6ab98382fa6164ad43e35fe1b170a5ceac
Marika <mscrepnek2004@gmail.com>
2026-06-04 20:41:03 -0700

    add activity type enum, add column t activity table of activity type

--- BeautyBuddy-backend/src/main/resources/db/migration/V6__feed_page_modifications.sql
+++ BeautyBuddy-backend/src/main/resources/db/migration/V6__feed_page_modifications.sql
@@ -6,8 +6,18 @@ DROP TABLE routine_image_activity;
 
 DROP TABLE wishlist_item_activity;
 
+CREATE TYPE activity_action_enum AS ENUM (
+    'CREATED',
+    'EDITED',
+    'ADDED',
+    'REMOVED',
+);
+
 ALTER TABLE activity
     ADD COLUMN target_id BIGINT;
 
 ALTER TABLE activity
     ALTER COLUMN target_id SET NOT NULL;
+
+ALTER TABLE activity
+    ADD COLUMN action activity_action_enum NOT NULL;
