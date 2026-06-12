3ffe92f9406dfb16a8698ca0f07e46b2d91f0b0f
Marika <mscrepnek2004@gmail.com>
2026-06-11 23:09:44 -0700

    use DTOs in activity service

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityService.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/community/activity/ActivityService.java
@@ -8,9 +8,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.beautybuddy.common.DTOMapper;
 import com.beautybuddy.community.activity.entity.Activity;
 import com.beautybuddy.community.activity.entity.ActivityType;
 import com.beautybuddy.community.activity.repo.ActivityRepository;
+import com.beautybuddy.community.dto.ActivityDTO;
 import com.beautybuddy.user.entity.User;
 import com.beautybuddy.user.entity.UserFollow;
 import com.beautybuddy.user.repo.FollowRepository;
@@ -26,31 +28,31 @@ public class ActivityService {
         this.followRepository = followRepository;
     }
 
-    public ResponseEntity<Activity> createActivity(User actor, ActivityType type, String payload) {
+    public ResponseEntity<Void> createActivity(User actor, ActivityType type, String payload) {
         Activity activity = new Activity();
         activity.setActor(actor);
         activity.setType(type);
         activity.setPayload(payload);
-        Activity savedActivity = activityRepository.save(activity);
-        return ResponseEntity.ok(savedActivity);
+        activityRepository.save(activity);
+        return ResponseEntity.ok().build();
     }
 
-    public Page<ResponseEntity<Activity>> getActivitiesByUsername(String username) {
+    public Page<ResponseEntity<ActivityDTO>> getActivitiesByUsername(String username) {
         Page<Activity> activities = activityRepository.findByActor_Username(username, PageRequest.of(0, 10));
-        return activities.map(ResponseEntity::ok);
+        return activities.map(DTOMapper::toActivityDTO).map(ResponseEntity::ok);
     }
 
-    public Page<ResponseEntity<Activity>> getFollowingActivitiesByUsername(String username) {
+    public Page<ResponseEntity<ActivityDTO>> getFollowingActivitiesByUsername(String username) {
         Page<UserFollow> following = followRepository.findByFollowerUsername(username, PageRequest.of(0, 1000));
         List<String> followedUsernames = following.getContent().stream()
                 .map(f -> f.getFollowed().getUsername()).collect(Collectors.toList());
 
         if (followedUsernames.isEmpty()) {
-            return Page.<ResponseEntity<Activity>>empty(PageRequest.of(0, 10));
+            return Page.<ResponseEntity<ActivityDTO>>empty(PageRequest.of(0, 10));
         }
 
         Page<Activity> activities = activityRepository.findByActor_UsernameIn(followedUsernames,
                 PageRequest.of(0, 10));
-        return activities.map(ResponseEntity::ok);
+        return activities.map(DTOMapper::toActivityDTO).map(ResponseEntity::ok);
     }
 }
