abbb400f316aead541443248a8a7c2d1d3547507
Marika <mscrepnek2004@gmail.com>
2026-06-26 12:06:48 -0700

    add pronoun and country enums to database

--- BeautyBuddy-backend/src/main/resources/db/migration/V7__profile_modifications.sql
+++ BeautyBuddy-backend/src/main/resources/db/migration/V7__profile_modifications.sql
@@ -2,3 +2,11 @@ ALTER TABLE account
     ADD COLUMN country VARCHAR(100),
     ADD COLUMN pronouns VARCHAR(50),
     ADD COLUMN date_of_birth DATE
+
+CREATE ENUM pronouns_enum AS ENUM ('he/him', 'she/her', 'they/them', 'other');
+
+CREATE ENUM country_enum AS ENUM (
+    'United States', 'Canada', 'United Kingdom', 'Australia', 'Germany', 
+    'France', 'Italy', 'Spain', 'India', 'China', 'Japan', 'Brazil', 
+    'Mexico', 'Russia', 'South Africa', 'Other'
+);
