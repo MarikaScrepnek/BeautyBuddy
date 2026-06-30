9a2ddbd7921b17c3faf6ad4af6cf024050d92cc4
Marika <mscrepnek2004@gmail.com>
2026-06-30 11:37:10 -0700

    map db enums to java enums

---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/user/enums/HairTextureEnum.java
@@ -0,0 +1,9 @@
+package com.beautybuddy.user.enums;
+
+public enum HairTextureEnum {
+    STRAIGHT,
+    WAVY,
+    CURLY,
+    COILY,
+    OTHER
+}
---
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/user/enums/SkinConditionEnum.java
@@ -0,0 +1,7 @@
+package com.beautybuddy.user.enums;
+
+public enum SkinConditionEnum {
+    SENSITIVE,
+    NORMAL,
+    ACNE_PRONE
+}
