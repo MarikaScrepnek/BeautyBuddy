8437eeb37e2eaaed1ed7a2519802f2efc22a5004
Marika <mscrepnek2004@gmail.com>
2026-07-10 13:41:06 -0700

    add profile info to usersearchdto

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/user/dto/UserSearchDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/user/dto/UserSearchDTO.java
@@ -1,11 +1,24 @@
 package com.beautybuddy.user.dto;
 
+import com.beautybuddy.user.enums.CountryEnum;
+import com.beautybuddy.user.enums.HairDensityEnum;
+import com.beautybuddy.user.enums.HairTextureEnum;
+import com.beautybuddy.user.enums.PronounEnum;
+import com.beautybuddy.user.enums.SkinConditionEnum;
+import com.beautybuddy.user.enums.SkinTypeEnum;
+
 public record UserSearchDTO(
         String username,
         String profilePictureUrl,
         boolean isFollowing,
         boolean isFollower,
-        boolean isCurrentUser
+        boolean isCurrentUser,
+        PronounEnum pronoun,
+        CountryEnum country,
+        HairDensityEnum hairDensity,
+        HairTextureEnum hairTexture,
+        SkinTypeEnum skinType,
+        SkinConditionEnum skinCondition
         ) {
 
 }
