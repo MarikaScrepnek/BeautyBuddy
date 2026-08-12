10b26b1e62d3ef615cd3c8c06af7e264afdab63a
Marika <mscrepnek2004@gmail.com>
2026-08-12 09:56:26 -0700

    pass enum[] in user searchDTO

--- BeautyBuddy-backend/src/main/java/com/beautybuddy/user/dto/UserSearchDTO.java
+++ BeautyBuddy-backend/src/main/java/com/beautybuddy/user/dto/UserSearchDTO.java
@@ -4,7 +4,7 @@ import com.beautybuddy.user.enums.CountryEnum;
 import com.beautybuddy.user.enums.HairDensityEnum;
 import com.beautybuddy.user.enums.HairTextureEnum;
 import com.beautybuddy.user.enums.PronounEnum;
-import com.beautybuddy.user.enums.SkinConditionEnum;
+import com.beautybuddy.user.enums.SkinConcernEnum;
 import com.beautybuddy.user.enums.SkinTypeEnum;
 
 public record UserSearchDTO(
@@ -18,7 +18,7 @@ public record UserSearchDTO(
         HairDensityEnum hairDensity,
         HairTextureEnum hairTexture,
         SkinTypeEnum skinType,
-        SkinConditionEnum skinCondition
+        SkinConcernEnum[] skinConcerns
         ) {
 
 }
